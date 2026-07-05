0e7722578174235882dcaa4abe84c9740f02e486
bom-b <sb97213@gmail.com>
2026-07-05 23:51:24 +0900

    init

---
+++ src/main/java/com/potatonetwork/potatocointracker/PotatoCoinTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.potatonetwork.potatocointracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoCoinTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoCoinTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/potatonetwork/potatocointracker/PotatoCoinTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatonetwork.potatocointracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoCoinTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
